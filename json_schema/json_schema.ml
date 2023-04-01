ea64d830eb18d3ca8744fea2c4add2b36154b894
Shon Feder <shon.feder@gmail.com>
2023-04-01 01:18:47 -0400

    Remove unneeded jsom_schema project

--- json_schema/json_schema.ml
+++
@@ -1,5 +0,0 @@
-(* type t = Json_schema_t.t *)
-module Json_schema_t = Json_schema_t
-module Json_schema_j = Json_schema_j
-(* let wrap json = Yojson.Safe.to_string json |> Json_schema_j.t_of_string *)
-(* let unwrap t = Json_schema_j.string_of_t t |> Yojson.Safe.from_string *)
