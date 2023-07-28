cacafd2aeecb02592a82e338195a73b1a3a17f54
Shon Feder <shon.feder@gmail.com>
2023-07-27 21:56:54 -0400

    Make openapi-gen a compenent instead of cram test
    This lest us use the library with all editor integration etc.

--- test/openapi-gen/test_openai.ml
+++ test/openapi-gen/test_openai.ml
@@ -9,6 +9,7 @@ module Api = Openai_api.Endpoint (Config)
 let main =
   let open Lwt.Syntax in
   let* res = Api.list_models () in
+  let* () = Lwt_io.printl "" in
   match res with
   | Error (_, s) ->
     let* () = Lwt_io.printl "Bad!" in
